86431036f96462589c2a25c45e1b13839b0399a8
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-08 18:42:11 -0500

    [actualizar] ajustes visuales

--- frontend/src/app/components/form-register/form-register.component.ts
+++ frontend/src/app/components/form-register/form-register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-form-register',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './form-register.component.html',
   styleUrl: './form-register.component.css'
 })
