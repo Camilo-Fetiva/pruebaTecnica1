17baae10a840a5572b36a027d36cc21ca5d687ff
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-08 18:11:02 -0500

    [actualizar] home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { Routes } from '@angular/router';
 // IMPORTAR RUTAS
 import { FormLoginComponent } from './components/form-login/form-login.component';
 import { FormRegisterComponent } from './components/form-register/form-register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {path: 'Ingresar', component: FormLoginComponent, title: 'Login'},
-    {path: 'Registrarse', component: FormRegisterComponent, title: 'Register'}
+    {path: 'Registrarse', component: FormRegisterComponent, title: 'Register'},
+    {path: '', component: HomeComponent, title: 'Home'}
 ];
---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
