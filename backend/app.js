2f379b4fc0c71db1941aff217a495ecdcceddfb9
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-07 21:35:32 -0500

    [actualizar] conexion con mongo

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import express from 'express'; //importar express para la prueba en la terminal
 import dotenv from 'dotenv'; //Dependencia para manejar variables de entorno
 import { connectionMongo } from './src/config/dataBase.js'; // dependencia para conectar la base de datos
 
+
 // CONFIGURAR EL USO DEL SERVIDOR CON MONGO
 const app = express();
 dotenv.config();
