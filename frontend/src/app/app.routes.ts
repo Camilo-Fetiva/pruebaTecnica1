2df1baa44129ae7d0c740cda3caad786463e92c8
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-08 17:53:29 -0500

    [actualizar] formularios y rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// IMPORTAR RUTAS
+import { FormLoginComponent } from './components/form-login/form-login.component';
+import { FormRegisterComponent } from './components/form-register/form-register.component';
+
+export const routes: Routes = [
+    {path: 'Ingresar', component: FormLoginComponent, title: 'Login'},
+    {path: 'Registrarse', component: FormRegisterComponent, title: 'Register'}
+];
