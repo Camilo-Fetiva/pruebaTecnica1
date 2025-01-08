6d5143890950c237e2d2f37950b86a0396cbaf58
Camilo-Fetiva <kmilofetiva@gmail.com>
2025-01-08 16:03:36 -0500

    [actualizar] conexion con mongo

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 // IMPORTAR LAS DEPENDENCIAS
 import express from 'express'; //importar express para la prueba en la terminal
 import dotenv from 'dotenv'; //Dependencia para manejar variables de entorno
-import { connectionMongo } from './src/config/dataBase.js'; // dependencia para conectar la base de datos
+import { connectionMongo } from './src/config/database.js'; // dependencia para conectar la base de datos
 
 
 // CONFIGURAR EL USO DEL SERVIDOR CON MONGO
@@ -18,5 +18,5 @@ connectionMongo();
 
 // EJECTUTAR EL PROYECTO EN EL PC
 app.listen(port, () => {
-    console.log ('Soy el server ejecutandose correctamente en el puerto ', port);
+    console.log ('Soy el server ejecutandose correctamente en el puerto', port);
 });
